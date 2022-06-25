8dd8fded8b7a5e558d3fcfdc31a991170e22c0bc
SaronTedrose <sarontedrose99@gmail.com>
2022-06-26 00:12:15 +0300

    Timer

---
+++ Timer/Timer/Form1.Designer.cs
@@ -0,0 +1,157 @@
+﻿namespace Timer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.lblOne = new System.Windows.Forms.Label();
+            this.lblTwo = new System.Windows.Forms.Label();
+            this.lblThree = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.timer3 = new System.Windows.Forms.Timer(this.components);
+            this.lblGoodLuck = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnStart
+            // 
+            this.btnStart.BackColor = System.Drawing.Color.Red;
+            this.btnStart.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnStart.Location = new System.Drawing.Point(95, 271);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(134, 29);
+            this.btnStart.TabIndex = 0;
+            this.btnStart.Text = "START";
+            this.btnStart.UseVisualStyleBackColor = false;
+            this.btnStart.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btnStop
+            // 
+            this.btnStop.BackColor = System.Drawing.Color.Red;
+            this.btnStop.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnStop.Location = new System.Drawing.Point(322, 271);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(142, 29);
+            this.btnStop.TabIndex = 1;
+            this.btnStop.Text = "STOP";
+            this.btnStop.UseVisualStyleBackColor = false;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // lblOne
+            // 
+            this.lblOne.AutoSize = true;
+            this.lblOne.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblOne.Location = new System.Drawing.Point(114, 88);
+            this.lblOne.Name = "lblOne";
+            this.lblOne.Size = new System.Drawing.Size(84, 91);
+            this.lblOne.TabIndex = 2;
+            this.lblOne.Text = "0";
+            // 
+            // lblTwo
+            // 
+            this.lblTwo.AutoSize = true;
+            this.lblTwo.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTwo.Location = new System.Drawing.Point(236, 88);
+            this.lblTwo.Name = "lblTwo";
+            this.lblTwo.Size = new System.Drawing.Size(84, 91);
+            this.lblTwo.TabIndex = 3;
+            this.lblTwo.Text = "0";
+            // 
+            // lblThree
+            // 
+            this.lblThree.AutoSize = true;
+            this.lblThree.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblThree.Location = new System.Drawing.Point(351, 88);
+            this.lblThree.Name = "lblThree";
+            this.lblThree.Size = new System.Drawing.Size(84, 91);
+            this.lblThree.TabIndex = 4;
+            this.lblThree.Text = "0";
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 500;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // timer2
+            // 
+            this.timer2.Interval = 1000;
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // timer3
+            // 
+            this.timer3.Interval = 1500;
+            this.timer3.Tick += new System.EventHandler(this.timer3_Tick);
+            // 
+            // lblGoodLuck
+            // 
+            this.lblGoodLuck.AutoSize = true;
+            this.lblGoodLuck.BackColor = System.Drawing.Color.CornflowerBlue;
+            this.lblGoodLuck.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblGoodLuck.Location = new System.Drawing.Point(130, 33);
+            this.lblGoodLuck.Name = "lblGoodLuck";
+            this.lblGoodLuck.Size = new System.Drawing.Size(191, 32);
+            this.lblGoodLuck.TabIndex = 5;
+            this.lblGoodLuck.Text = "GOOD LUCK";
+            this.lblGoodLuck.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.ClientSize = new System.Drawing.Size(587, 404);
+            this.Controls.Add(this.lblGoodLuck);
+            this.Controls.Add(this.lblThree);
+            this.Controls.Add(this.lblTwo);
+            this.Controls.Add(this.lblOne);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.btnStart);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.Label lblOne;
+        private System.Windows.Forms.Label lblTwo;
+        private System.Windows.Forms.Label lblThree;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.Timer timer3;
+        private System.Windows.Forms.Label lblGoodLuck;
+    }
+}
+
---
+++ Timer/Timer/Form1.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Timer
+{
+    public partial class Form1 : Form
+    {
+         
+     
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            timer1.Start();
+            timer2.Start();
+            timer3.Start();
+
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            timer1.Stop();
+            timer2.Stop();
+            timer3.Stop();
+            if (lblOne.Text==lblTwo.Text && lblTwo.Text == lblThree.Text)
+            {
+                MessageBox.Show("WINNER!! Congradulation");
+            }
+            else
+            {
+                MessageBox.Show("You Lost!! Try Again");
+            }
+        }
+      
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+           
+                int time1 = int.Parse(lblOne.Text);
+            if (time1 < 9)
+            {
+                time1++;
+            }
+            else
+            {
+                time1 = 0;
+            }
+                lblOne.Text = time1.ToString();
+            
+
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+          
+                int time2 = int.Parse(lblTwo.Text);
+            if (time2 < 9)
+            {
+                time2++;
+            }
+            else
+            {
+                time2 = 0;
+            }
+                lblTwo.Text = time2.ToString();
+            
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void timer3_Tick(object sender, EventArgs e)
+        {
+           
+            
+                int time3 = int.Parse(lblThree.Text);
+            if (time3 < 9)
+            {
+                time3++;
+            }
+            else
+            {
+                time3= 0;
+            }
+                lblThree.Text = time3.ToString();
+            
+           
+        }
+
+       
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            
+
+
+        }
+    }
+}
